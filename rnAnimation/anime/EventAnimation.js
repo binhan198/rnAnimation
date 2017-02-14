192e5c2f0db1e5574fc836a948ec4052735cd3fd
周彬涵 <zhoubinhan@chinamobile.com>
2017-02-14 16:47:10 +0800

    添加event动画

--- rnAnimation/anime/EventAnimation.js
+++ rnAnimation/anime/EventAnimation.js
@@ -7,11 +7,32 @@ import {
   Image,
   TextInput,
   Animated,
+  PanResponder,
 } from 'react-native';
 
 import BaseAnimation from './BaseAnimation';
 
 export default class EventAnimation extends BaseAnimation {
+  constructor(props) {
+    super(props);
+    this.state = {
+      panXY : new Animated.ValueXY()
+    };
+    this.panResponder = PanResponder.create({
+      onStartShouldSetPanResponder : () => true,
+      onMoveShouldSetPanResponder : () => true,
+      onPanResponderMove : Animated.event([null, {
+        dx : this.state.panXY.x,
+        dy : this.state.panXY.y,
+      }]),
+      onPanResponderRelease : () => {
+        Animated.spring(
+          this.state.panXY,
+          {toValue : {x : 0, y : 0}} // Back to zero
+        ).start();
+      },
+    });
+  }
 
   render() {
     return (
@@ -19,6 +40,11 @@ export default class EventAnimation extends BaseAnimation {
         <View style = {styles.backBtn}>
           {super.render()}
         </View>
+        <Animated.Image 
+          resizeMode = {'stretch'} 
+          source = {{uri : 'react_native_logo'}} 
+          {...this.panResponder.panHandlers} 
+          style = {[styles.animatedImage, {left : this.state.panXY.x, top : this.state.panXY.y}]}/>
       </View>
     );
   }
@@ -34,6 +60,11 @@ const styles = StyleSheet.create({
     alignSelf:'flex-start',
     marginLeft:15,
     marginTop:20,
+  },
+  animatedImage:{
+    width : 200,
+    height : 200, 
+    marginTop : 120,
   },
 });
 
